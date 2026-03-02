76fb2e5d0351bf1fa34fc37a2abee3ef13bb15dc
es7ray <JessJake@proton.me>
2026-03-03 02:03:42 +0300

    Fetch: Add spending time logging into Informer

--- Informer/Worker.cs
+++ Informer/Worker.cs
@@ -6,81 +6,103 @@ namespace Informer;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-    
     private readonly IBus _bus;
+    private readonly ILogger<Worker> _logger;
 
     private readonly Settings _settings;
-    
+
     public Worker(ILogger<Worker> logger, IBus bus)
     {
         _logger = logger;
         _bus = bus;
-        _settings  = new Settings(); 
-        
+        _settings = new Settings();
     }
 
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         // Created only if doesn't exist
         Directory.CreateDirectory(_settings.WorkDirectory);
         Directory.CreateDirectory(_settings.ArchiveDirectory);
-        
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            var files = Directory.GetFiles(_settings.WorkDirectory).Where(x=> _settings.AllSupportedFormats.Contains(Path.GetExtension(x)));
-            foreach (var filePath in files)
-            {
-                // In Real project we need to store files on shared directory
-                // But it's just a project for checking microservices
-                var fileName = Path.GetFileName(filePath);
-                var archiveFilePath = Path.Combine(_settings.ArchiveDirectory, fileName);
-                var ext = Path.GetExtension(filePath);
-
-                var contentType = ContentType.Unknown;
+            var processId = Guid.NewGuid();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             
-                if (_settings.TextSupportedFormats.Contains(ext))
-                    contentType = ContentType.Text;
-                else if (_settings.ImageSupportedFormats.Contains(ext))
-                    contentType = ContentType.Image;
+            var files = Directory.GetFiles(_settings.WorkDirectory)
+                .Where(x => _settings.AllSupportedFormats.Contains(Path.GetExtension(x))).ToList();
 
-                IContentReport? report = null;
-                var reportId = Guid.NewGuid();
-                
-                _logger.LogInformation("{reportId} with fileName {fileName} processing..", reportId, fileName);
-                
-                switch (contentType)
-                {
-                    case ContentType.Text:
-                        report = new TextContentReport(
-                            reportId,
-                            fileName,
-                            await File.ReadAllTextAsync(filePath, stoppingToken),
-                            DateTime.UtcNow);
-                        
-                        await _bus.Publish((TextContentReport)report, stoppingToken);
-                        break;
-                    
-                    case ContentType.Image:
-                        report = new ImageContentReport(
-                            reportId,
-                            fileName,
-                            archiveFilePath,  
-                            DateTime.UtcNow);
-                        
-                        await _bus.Publish((ImageContentReport)report, stoppingToken);
-                        break;
-                }
-                
-                if (report == null)
-                    continue;
-                
-                File.Move(filePath, archiveFilePath, overwrite: true);
-                _logger.LogInformation("{reportId} with fileName {fileName} processed", reportId, fileName);
+            if (files.Count != 0)
+            {
+                _logger.LogInformation("{processId} Processing {count} files...", processId, files.Count());
+            }
+            
+            foreach (var file in files)
+            {
+                await ProcessFile(file, stoppingToken);
+            }
+
+            watch.Stop();
+            if (files.Count != 0)
+            {
+                var totalSeconds = watch.Elapsed.TotalSeconds;
+                var filesPerSecond = totalSeconds > 0 ? Math.Round(files.Count / totalSeconds, 2) : 0;
+
+                _logger.LogInformation("{processId} {count} files processed by {ms} m. Speed: {speed} f/sec", 
+                    processId, files.Count, watch.ElapsedMilliseconds, filesPerSecond);
             }
             
             // in real projects use FileSystemWatcher instead of this 
-            await Task.Delay(1000 * 5, stoppingToken); 
+            await Task.Delay(1000 * 5, stoppingToken);
         }
+    }
+    
+    // In real project just take it in service, but there is no need
+    private async Task ProcessFile(string filePath, CancellationToken stoppingToken)
+    {
+        // In Real project we need to store files on shared directory
+        var fileName = Path.GetFileName(filePath);
+        var archiveFilePath = Path.Combine(_settings.ArchiveDirectory, fileName);
+        var ext = Path.GetExtension(filePath);
+
+        var contentType = ContentType.Unknown;
+
+        if (_settings.TextSupportedFormats.Contains(ext))
+            contentType = ContentType.Text;
+        else if (_settings.ImageSupportedFormats.Contains(ext))
+            contentType = ContentType.Image;
+
+        IContentReport? report = null;
+        var reportId = Guid.NewGuid();
+        
+        switch (contentType)
+        {
+            case ContentType.Text:
+                report = new TextContentReport(
+                    reportId,
+                    fileName,
+                    await File.ReadAllTextAsync(filePath, stoppingToken),
+                    DateTime.UtcNow);
+
+                await _bus.Publish((TextContentReport)report, stoppingToken);
+                break;
+
+            case ContentType.Image:
+                report = new ImageContentReport(
+                    reportId,
+                    fileName,
+                    archiveFilePath,
+                    DateTime.UtcNow);
+
+                await _bus.Publish((ImageContentReport)report, stoppingToken);
+                break;
+        }
+
+        if (report == null)
+            return;
+
+        File.Move(filePath, archiveFilePath, true);
+       
     }
 }
